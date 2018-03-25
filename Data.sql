cb72baa99966580563c045c9f6d71ac1ccd2dcc7
Raghav Puri <raghavpuri31@gmail.com>
2018-03-25 09:41:34 +0530

    Create Data.sql

---
+++ Data.sql
@@ -0,0 +1,56 @@
+SET SQL_SAFE_UPDATES=0;
+
+delete from `brs`.`reserve`;
+
+delete from `brs`.`bus`;
+ALTER TABLE `brs`.`bus` AUTO_INCREMENT = 200;
+
+delete from `brs`.`route`;
+ALTER TABLE `brs`.`route` AUTO_INCREMENT = 10;
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Delhi','Hyderabad');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Hyderabad','Delhi');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Mumbai','Bangalore');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Bangalore','Mumbai');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Visakhapatnam','Amritsar');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Amritsar','Visakhapatnam');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Bhopal','Bangalore');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Bangalore','Bhopal');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Hyderabad','Tirupati');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Tirupati','Hyderabad');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Bangalore','Tirupati');
+
+INSERT INTO `brs`.`route` (origin,destination) VALUES ('Tirupati','Bangalore');
+
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (10,1,1600,'20:00','09:00');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (10,0,1700,'20:00','06:30');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (10,1,2000,'21:00','07:00');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (11,0,1500,'18:00','05:00');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (12,0,1200,'20:00','09:00');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (12,1,2100,'20:00','18:00');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (13,0,1700,'21:30','19:00');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (13,0,800,'14:00','09:30');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (13,1,700,'20:00','19:00');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (14,0,300,'20:00','09:00');
+
+INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (13,1,1800,'19:10','17:00');
